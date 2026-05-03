5901e211326891639b583a531f4ea8dfe747eaa1
Nicolas Conde Salazar <ncondesalazar@gmail.com>
2026-05-03 18:43:43 -0500

    chore: add privacy field to user boards endpoint (#48)

--- internal/domain/board.go
+++ internal/domain/board.go
@@ -22,8 +22,9 @@ type Board struct {
 }
 
 type UserBoardListItem struct {
-	ID   string `json:"id" example:"123e4567-e89b-12d3-a456-426614174000"`
-	Name string `json:"name" example:"My Board"`
+	ID      string       `json:"id" example:"123e4567-e89b-12d3-a456-426614174000"`
+	Name    string       `json:"name" example:"My Board"`
+	Privacy BoardPrivacy `json:"privacy" example:"private"`
 }
 
 type BoardDetails struct {
